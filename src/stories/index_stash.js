2fc6c43297082b3cff77a157f4f66e741420ef46
Jordan Blakey <jordan@jordanblakey.com>
2017-05-11 20:48:54 -0500

    first lesson

---
+++ src/stories/index_stash.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@kadira/storybook';
+// import MainSection from '../components/MainSection';
+
+const stories = storiesOf('MainSection', module);
+
+stories.add('with a single todo item', () => (
+    <MainSection
+        todos={[{id: '1', text: 'Learn React'}]}
+        actions={{}}
+    />
+))
